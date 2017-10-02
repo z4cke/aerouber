dfce2bf03a34a70423e0ad8c3dc798e3fb959085
Arminmosaffaie <32455979+Arminmosaffaie@users.noreply.github.com>
2017-10-03 01:52:54 +0200

    Add files via upload

---
+++ aeroGUI.h
@@ -0,0 +1,37 @@
+#pragma once
+class aeroGUI
+{
+private:
+	char requestPath;
+	// browser button netbeans library
+	// start button netbeans library
+	// textField button netbeans library
+	// info Label button netbeans library
+	// title
+
+public:
+	void openGUI() 
+	{
+
+		/**
+		
+		* Start GUI
+
+		*/
+
+
+
+	}
+	char getPath() 
+	{
+		/**
+
+		* returns path
+
+		*/
+
+
+	}
+
+};
+
