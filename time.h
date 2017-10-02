0366d9b120d87cdaea0d3d1cafa0afea539f976e
Arminmosaffaie <32455979+Arminmosaffaie@users.noreply.github.com>
2017-10-03 01:46:27 +0200

    Add files via upload

---
+++ time.h
@@ -0,0 +1,58 @@
+#pragma once
+#ifndef TIME_H
+#define TIME_H
+
+/*
+ 
+ Time object is used to calculate and evaluate flight scheduling for passengers. 
+
+
+*/
+
+class time
+{
+private:
+
+/*
+
+*int h, m, s are used to store following hour, minute and seconds. 
+
+*/
+	int h, m, s;
+
+public:
+	/**
+	*
+	
+	*/
+
+	
+	int getHour() 
+	{
+		/*
+
+		* Returns hour int
+
+		*/
+
+	}
+	int getMin() 
+	{
+		/*
+
+		* Returns minute int
+
+		*/
+
+	}
+
+};
+	double getTime() 
+	{
+	/*
+	
+	* Returns the time hh:mm
+	
+	*/
+
+	}
